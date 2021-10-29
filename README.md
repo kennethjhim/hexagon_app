43ad1eef029dad8d1aecae6b56482b75383aa6c5
Kenneth Infante <infantekennethjhim@gmail.com>
2021-10-29 16:10:15 +0800

    Step 1 - added first test

---
+++ README.md
@@ -0,0 +1,12 @@
+### Overview
+
+This is the hexagonal app tutorial using Rust. The features of Rust enables data-driven programming and naturally leads to cleaner design.
+This is the future of programming.
+
+#### Step 1
+
+Use cases
+    * Create a Pokemon
+    * Read all Pokemons
+    * Read one Pokemon
+    * Delete a Pokemon
---
+++ src/domain/create_pokemon.rs
@@ -0,0 +1,30 @@
+// plain old data
+struct Request {
+    number: u16,
+    name: String,
+    types: Vec<String>,
+}
+
+fn execute(req: Request) -> u16 {
+    req.number
+}
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_should_return_the_pokemon_number_otherwise() {
+        let number = 25;
+        let req = Request {
+            number,
+            name: String::from("Pikachu"),
+            types: vec![String::from("Electric")],
+        };
+
+        let res = execute(req);
+
+        assert_eq!(res, number)
+    }
+}
