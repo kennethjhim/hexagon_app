009635f7cc5b7839562aab14831c1d84ae8a443f
Kenneth Infante <infantekennethjhim@gmail.com>
2021-10-31 20:50:27 +0800

    Stage 6 - completed the SQLite repository

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,40 @@ mod repositories;
 extern crate rouille;
 extern crate serde;
 
-use repositories::pokemon::InMemoryRepository;
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg};
+use repositories::pokemon::{Repository, InMemoryRepository, SqliteRepository};
 use std::sync::Arc;
 
 fn main() {
-    let repo = Arc::new(InMemoryRepository::new());
-    api::serve("localhost:8000", repo);
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        // .arg(Arg::with_name("cli").long("cli").help("Runs in CLI mode"))
+        .arg(Arg::with_name("sqlite").long("sqlite").value_name("PATH"))
+        .get_matches();
+
+    let repo = build_repo(matches.value_of("sqlite"));
+
+    match matches.occurrences_of("gui") {
+        0 => api::serve("localhost:8000", repo),
+        _ => unreachable!(),
+    }
+}
+
+fn build_repo(sqlite_value: Option<&str>) -> Arc<dyn Repository> {
+
+    if let Some(path) = sqlite_value {
+        match SqliteRepository::try_new(path) {
+            Ok(repo) => return Arc::new(repo),
+            _ => panic!("Error while creating sqlite repo"),
+        }
+    }
+    // default repo
+    match SqliteRepository::try_new("./database.db") {
+        Ok(default_repo) => return Arc::new(default_repo),
+        _ => panic!("Error accessing an existing database"),
+    }
 }
