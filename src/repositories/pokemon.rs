0088db25a4a236ab5fd722eed17a970194082b25
Kenneth Infante <infantekennethjhim@gmail.com>
2021-11-01 11:30:19 +0800

    Added the airtable but has errors

--- src/repositories/pokemon.rs
+++ src/repositories/pokemon.rs
@@ -1,4 +1,5 @@
 use crate::domain::entities::{Pokemon, PokemonName, PokemonNumber, PokemonTypes};
+use serde::Deserialize;
 use std::sync::{Mutex, MutexGuard};
 use rusqlite::{Connection, OpenFlags, params_from_iter, Error::SqliteFailure, params};
 
@@ -43,10 +44,6 @@ pub trait Repository: Send + Sync {
 pub struct InMemoryRepository {
     error: bool,
     pokemons: Mutex<Vec<Pokemon>>,
-}
-
-pub struct SqliteRepository {
-    connection: Mutex<Connection>,
 }
 
 impl InMemoryRepository {
@@ -141,6 +138,10 @@ impl Repository for InMemoryRepository {
         lock.remove(index);
         Ok(())
     }
+}
+
+pub struct SqliteRepository {
+    connection: Mutex<Connection>,
 }
 
 impl SqliteRepository {
@@ -342,6 +343,172 @@ impl Repository for SqliteRepository {
             params![u16::from(number)],
         ) {
             Ok(0) => Err(DeleteError::NotFound),
+            Ok(_) => Ok(()),
+            _ => Err(DeleteError::Unknown),
+        }
+    }
+}
+
+pub struct AirtableRepository {
+    url: String,
+    auth_header: String,
+}
+
+#[derive(Deserialize)]
+struct AirtableJson {
+    records: Vec<AirtableRecord>,
+}
+
+#[derive(Deserialize)]
+struct AirtableRecord {
+    id: String,
+    fields: AirtableFields,
+}
+
+#[derive(Deserialize)]
+struct AirtableFields {
+    number: u16,
+    name: String,
+    types: Vec<String>,
+}
+
+impl AirtableRepository {
+    pub fn try_new(api_key: &str, base_id: &str) -> Result<Self, ()> {
+        let url = format!("https://api.airtable.com/v0/{}/pokemons", base_id);
+        let auth_header = format!("Bearer {}", api_key);
+
+        if let Err(_) = ureq::get(&url).set("Authorization", &auth_header).call() {
+            return Err(());
+        }
+
+        Ok(Self { url, auth_header })
+    }
+
+    fn fetch_pokemon_rows(&self, number: Option<u16>) -> Result<AirtableJson, ()> {
+        let url = match number {
+            Some(number) => format!("{}?filterByFormula=number%3D{}", self.url, number),
+            None => format!("{}?sort%5B0%5D%5Bfield%5D=number&sort%5B0%5D%5Bdirection%5D=desc", self.url),
+            // None => format!("{}?maxRecords=3&view=Grid%20view", self.url),
+            // None => format!("{}", self.url),
+        };
+
+        let res = match ureq::get(&url)
+            .set("Authorization", &self.auth_header)
+            .call()
+        {
+            Ok(res) => res,
+            _ => return Err(()),
+        };
+
+        match res.into_json::<AirtableJson>() {
+            Ok(json) => Ok(json),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Repository for AirtableRepository {
+    fn insert(
+        &self,
+        number: PokemonNumber,
+        name: PokemonName,
+        types: PokemonTypes,
+    ) -> Result<Pokemon, InsertError> {
+        let json = match self.fetch_pokemon_rows(Some(u16::from(number.clone()))) {
+            Ok(json) => json,
+            _ => return Err(InsertError::Unknown),
+        };
+
+        if !json.records.is_empty() {
+            return Err(InsertError::Conflict);
+        }
+
+        let body = ureq::json!({
+            "records": [{
+                "fields": {
+                    "number": u16::from(number.clone()),
+                    "name": String::from(name.clone()),
+                    "types": Vec::<String>::from(types.clone()),
+                },
+            }],
+        });
+
+        if let Err(_) = ureq::post(&self.url)
+            .set("Authorization", &self.auth_header)
+            .send_json(body)
+        {
+            return Err(InsertError::Unknown);
+        }
+
+        Ok(Pokemon::new(number, name, types))
+    }
+
+    fn fetch_all(&self) -> Result<Vec<Pokemon>, FetchAllError> {
+        let json = match self.fetch_pokemon_rows(None) {
+            Ok(json) => json,
+            _ => return Err(FetchAllError::Unknown),
+        };
+
+        let mut pokemons = vec![];
+
+        for record in json.records.into_iter() {
+            match (
+                PokemonNumber::try_from(record.fields.number),
+                PokemonName::try_from(record.fields.name),
+                PokemonTypes::try_from(record.fields.types),
+            ) {
+                (Ok(number), Ok(name), Ok(types)) => {
+                    pokemons.push(Pokemon::new(number, name, types))
+                }
+                _ => {
+                    // _ => return Err(FetchAllError::Unknown),
+                    // warn!("An error: {}; skipped.", FetchAllError::Unknown);
+                    continue;
+                }
+            }
+        }
+
+        Ok(pokemons)
+    }
+
+    fn fetch_one(&self, number: PokemonNumber) -> Result<Pokemon, FetchOneError> {
+        let mut json = match self.fetch_pokemon_rows(Some(u16::from(number.clone()))) {
+            Ok(json) => json,
+            _ => return Err(FetchOneError::Unknown),
+        };
+
+        if json.records.is_empty() {
+            return Err(FetchOneError::NotFound);
+        }
+
+        let record = json.records.remove(0);
+
+        match (
+            PokemonNumber::try_from(record.fields.number),
+            PokemonName::try_from(record.fields.name),
+            PokemonTypes::try_from(record.fields.types),
+        ) {
+            (Ok(number), Ok(name), Ok(types)) => Ok(Pokemon::new(number, name, types)),
+            _ => Err(FetchOneError::Unknown),
+        }
+    }
+
+    fn delete(&self, number: PokemonNumber) -> Result<(), DeleteError> {
+        let mut json = match self.fetch_pokemon_rows(Some(u16::from(number.clone()))) {
+            Ok(json) => json,
+            _ => return Err(DeleteError::Unknown),
+        };
+
+        if json.records.is_empty() {
+            return Err(DeleteError::NotFound);
+        }
+
+        let record = json.records.remove(0);
+
+        match ureq::delete(&format!("{}/{}", self.url, record.id))
+            .set("Authorization", &self.auth_header)
+            .call()
+        {
             Ok(_) => Ok(()),
             _ => Err(DeleteError::Unknown),
         }
